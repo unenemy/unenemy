fae26154674998f46b4678353b2fe452e650ab8c
Dmitry Grib <unenemy@gmail.com>
2013-11-07 11:15:49 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Unenemy::Application.config.secret_key_base = '7c25ac55365fe907d6fc8e4b2046b4dc8cb3f9b605f25433d497ff759383ab3afff06927c077391223305467c5828d9160ca4edbf53125ac2bc6da0d124d0502'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Unenemy::Application.config.session_store :cookie_store, key: '_unenemy_session'
